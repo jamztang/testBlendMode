666355db0c7be5744e63ad368a7ad920779ab906
James Tang <james@goodnotesapp.com>
2022-03-01 19:48:34 +0800

    Initial Commit

---
+++ testBlendMode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testBlendMode
+//
+//  Created by James Tang on 1/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
